f0d432828169b186d20af41ffb004c96dd7c85ed
mmilica95 <milica095@gmail.com>
2019-06-13 15:39:11 +0200

    Login sa tokenom

--- Front-end/src/app/services/user.service.ts
+++ Front-end/src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from 'src/models/korisnik';
 
 @Injectable({
@@ -27,5 +27,11 @@ export class UserService {
     // 1 - korisnik, 2 - admin, 3 - kontroler, ostalo - neregistrovani korisnik
     this.userRole = 2;
     return this.userRole;
+  }
+
+  userAuthentication(Email, Password){
+    var data = "UserName=" + Email + "&Password=" + Password + "&grant_type=password";
+    var reqHeader = new HttpHeaders({ 'Content-Type': 'application/x-www-urlencoded','No-Auth':'True' });
+    return this._http.post(this._baseUrl + '/oauth/token', data, { headers: reqHeader });
   }
 }
--- Front-end/src/app/shared/header/header.component.ts
+++ Front-end/src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,10 +9,16 @@ import { Component, OnInit, Input } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   @Input() elements;
+  routerLinkVariable;
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+  LogOutButtonClick(){
+    localStorage.removeItem('userToken');
+    this.router.navigate(['/login']);
   }
 
 }
